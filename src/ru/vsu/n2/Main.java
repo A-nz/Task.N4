81d3a6ec68269bf18f0c31e81468dd3e5072ae11
Petrov Andrey <pandr2002@mail.ru>
2020-11-03 20:28:05 +0300

    ..

--- src/ru/vsu/n2/Main.java
+++ src/ru/vsu/n2/Main.java
@@ -5,33 +5,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double a = getDouble("a");
-        double h = getDouble("h");
-        int n = getInteger("n");
-        printResult(getFunctionValue(a, h, n));
+        double a = readDouble("a");
+        double h = readDouble("h");
+        int n = readInteger("n");
+        printResult(calcGx(a, h, n));
     }
 
-    private static double getFunctionValue(double a, double h, double n) {
+    private static double calcGx(double a, double h, double n) {
         double result = 0;
         for (int i = 0; i <= n; i++) {
-            result = (getFunktion(a + i * h)) + result;
+            result = (calcFx(a + i * h)) + result;
         }
         return result;
     }
 
-    private static double getDouble(String NameOfValue) {
+    private static double readDouble(String NameOfValue) {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("%s = ", NameOfValue);
         return scanner.nextDouble();
     }
 
-    private static int getInteger(String NameOfValue) {
+    private static int readInteger(String NameOfValue) {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("%s = ", NameOfValue);
         return scanner.nextInt();
     }
 
-    private static double getFunktion(double x) {
+    private static double calcFx(double x) {
         return (((x * x) + 1) * Math.cos(x));
     }
 
